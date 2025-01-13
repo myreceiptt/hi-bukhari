4078f8a9142f41a15619acfd1de1d2211193e757
myreceiptt <nota@straight-line.org>
2025-01-13 11:57:41 +0700

    online now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "Bukhari Islamic Art Gallery - Harmoni Istiqlal",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "Galeri Seni Islam Bukhari",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import Link from "next/link";
-import { Header } from "@/components/header/Header";
-import { Footer } from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <div className="py-20">
-      <Header 
-        title="Web3 Developer Course"
-        subtitle="Learn how to build web3 applications with the Thirdweb Connect SDK."
-      />
       <Menu />
-      <Footer/>
     </div>
   );
 }
@@ -49,19 +42,18 @@ function Menu() {
         description="Learn how get data and interact with the blockchain with a connected wallet."
       />
     </div>
-  )
+  );
 }
 
 function MenuItem(props: { title: string; href: string; description: string }) {
   return (
     <Link
       href={props.href}
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
+      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700">
       <article>
         <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
         <p className="text-sm text-zinc-400">{props.description}</p>
       </article>
     </Link>
-  )
+  );
 }

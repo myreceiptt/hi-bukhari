4b23bfaacdfbddeedd0f857ac03cab748c7fc87a
myreceiptt <nota@straight-line.org>
2025-01-13 16:40:51 +0700

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,100 @@
-import Link from "next/link";
+"use client";
+import { ConnectButton, ConnectEmbed, useActiveAccount } from "thirdweb/react";
+import { ethereum, polygon, base, baseSepolia } from "thirdweb/chains";
+import { createWallet, inAppWallet } from "thirdweb/wallets";
+import { client } from "./client";
+import { SignInLayout } from "@/components/SignInLayout";
 
-export default function Home() {
+const ConnectEmbedPage: React.FC = () => {
   return (
-    <div className="py-20">
-      <Menu />
+    <div className="p-2">
+      <SignInLayout>
+        <ConnectEmbeds />
+      </SignInLayout>
+    </div>
+  );
+};
+
+function ConnectEmbeds() {
+  return (
+    <div className="grid justify-center">
+      <CustomThemeConnectEmbed />
     </div>
   );
 }
 
-function Menu() {
+// Customize modal theme in ConnectEmbed
+function CustomThemeConnectEmbed() {
+  // Check if wallet is connected
+  const account = useActiveAccount();
+
+  const dompets = [
+    inAppWallet({
+      auth: {
+        options: ["google", "email", "passkey", "apple"],
+      },
+    }),
+    createWallet("io.metamask"),
+    createWallet("app.phantom"),
+    createWallet("com.okex.wallet"),
+    createWallet("com.coinbase.wallet"),
+  ];
+
   return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center px-10">
-      <MenuItem
-        title="ConnectButton"
-        href="/connect-button"
-        description="Learn what our ConnectButton UI component is, how to use it, and how to customize it."
+    <div className="flex flex-col items-center mb-20 md:mb-20">
+      <p className="text-base mb-4 md:mb-4">Social Login</p>
+      <ConnectEmbed
+        client={client}
+        // chain={baseSepolia}
+        chains={[ethereum, polygon, base, baseSepolia]}
+        wallets={dompets}
+        accountAbstraction={{
+          factoryAddress: "0x82EC684C86b84AC60b5e162EC87d6DCF4213D468",
+          chain: baseSepolia,
+          sponsorGas: true,
+        }}
+        // Customize modal theme
+        style={{}}
+        // theme={darkTheme({
+        //   colors: {
+        //     primaryText: "#F6F8FF",
+        //     secondaryText: "#2B333D",
+        //     accentText: "#F6F8FF",
+        //     modalOverlayBg: "#DAE8FC",
+        //     modalBg: "#010101",
+        //     accentButtonBg: "#2469DA",
+        //     accentButtonText: "#F6F8FF",
+        //     secondaryButtonBg: "#010101",
+        //     secondaryButtonText: "#F6F8FF",
+        //     secondaryButtonHoverBg: "#2469DA",
+        //     separatorLine: "#2B333D",
+        //     borderColor: "#2B333D",
+
+        //     primaryButtonBg: "#2469DA",
+        //     primaryButtonText: "#F6F8FF",
+
+        //     connectedButtonBg: "#010101",
+        //     connectedButtonBgHover: "#2469DA",
+        //   },
+        //Customize font
+        //   fontFamily: "Arial Black",
+        // })}
+        privacyPolicyUrl="/#"
+        termsOfServiceUrl="/#"
+        showThirdwebBranding={false}
+        // connectModal={{
+        //   size: "compact",
+        //   title: "Please LOG IN!",
+        //   titleIcon: "/memora-logo.gif",
+        //   showThirdwebBranding: false,
+        //   termsOfServiceUrl: "/terms",
+        //   privacyPolicyUrl: "/policy",
+        // }}
       />
-      <MenuItem
-        title="ConnectEmbed"
-        href="/connect-embed"
-        description="Learn what our ConnectEmbed UI component is, how to use it, and how to customize it."
-      />
-      <MenuItem
-        title="In-App Wallet"
-        href="/in-app-wallets"
-        description="Learn what our In-App Wallets are and how to implement them in your application."
-      />
-      <MenuItem
-        title="Custom Login Flow"
-        href="/custom-login-flow"
-        description="Learn how to use Connect SDK to build your own custom login flow."
-      />
-      <MenuItem
-        title="Account Abstraction"
-        href="/account-abstraction"
-        description="Learn how to implement account abstraction with ConnectButton."
-      />
-      <MenuItem
-        title="Wallet Connection"
-        href="/wallet-connection"
-        description="Learn how get data and interact with the blockchain with a connected wallet."
-      />
+      {/* Show ConnectButton in connect state when wallet is connected */}
+      {account && <ConnectButton client={client} />}
     </div>
   );
 }
 
-function MenuItem(props: { title: string; href: string; description: string }) {
-  return (
-    <Link
-      href={props.href}
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700">
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </Link>
-  );
-}
+export default ConnectEmbedPage;

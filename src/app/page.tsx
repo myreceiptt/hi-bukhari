8a0d49755e0e75b02200cf6031c58ff374b584a3
myreceiptt <nota@straight-line.org>
2025-01-15 14:30:39 +0700

    tekekss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import { ConnectButton, ConnectEmbed, useActiveAccount } from "thirdweb/react";
 import { detailsButton } from "@/config/details";
 import { dompets } from "@/config/dompets";
 import { tokeks } from "@/config/tokeks";
-import { tekeks } from "@/config/tekeks";
 import { client } from "@/config/client";
 
 // Components
@@ -49,7 +48,18 @@ const ConnectEmbedPage: React.FC = () => {
             detailsModal={{
               assetTabs: ["token", "nft"],
             }}
-            supportedNFTs={tekeks}
+            supportedNFTs={{
+              [polygon.id]: [
+                "0x0015C1dEb48c3aD0f5427cBbE81Cb36366F1621D", // MEMORA ONE
+              ],
+              [base.id]: [
+                "0x1925B991C5e2eC45BA1f34786BAd405d58202140", // MEMORA ONE
+              ],
+              [baseSepolia.id]: [
+                "0xc3046681149f96746b362a64472fD4B1cd1E33B2", // MEMORA ZER0
+                "0x045C2bC19d61B7527B1d996548B67B2Fa8cD68e1", // Bukhari Virtual Collectibles
+              ],
+            }}
           />
         </div>
         <div className="flex flex-col gap-4 content-normal px-0 md:px-20 m-4">
